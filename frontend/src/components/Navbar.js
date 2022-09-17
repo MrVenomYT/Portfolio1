a614730cd74425ede493d657b6a90769fe333820
Muhammad Hasil <t3ch91cal.h3r0@gmail.com>
2022-09-17 18:44:48 +0500

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
  <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/resume"
+                to="https://business.fiverr.com/venomdesigne613"
                 onClick={() => updateExpanded(false)}
               >
                 <HiRefresh style={{ marginBottom: "2px" }} /> HireMe
