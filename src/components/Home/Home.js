d23a5413d2102ff2662d91c4233da235b1d76ce3
Muhammad Hasil <t3ch91cal.h3r0@gmail.com>
2024-12-04 00:06:05 +0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import "../../index.css";
+import "../../style.css";
 import { motion } from "framer-motion";
 
 function Home() {
