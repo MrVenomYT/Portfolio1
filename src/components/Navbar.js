0d7a6cdc0337d342a30fca6ba55535a95a465173
Muhammad Hasil <t3ch91cal.h3r0@gmail.com>
2024-12-04 00:09:13 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,16 +79,6 @@ function NavBar() {
                 />Projects
               </Nav.Link>
             </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
